d71c3972d675cc0994ebd50b207f40a22e4a6bb2
Tim Schartman <tschartman@yahoo.com>
2019-07-09 23:15:21 -0500

    recieve text message then post to api

--- server.js
+++ server.js
@@ -1,26 +1,36 @@
+var util = require('util')
+const axios = require('axios')
 const http = require('http');
 const express = require('express');
-const MessagingResponse = require('twilio').twiml.MessagingResponse;
+const bodyParser = require('body-parser')
 var port = process.env.PORT || 8080;
 
+const app = express()
 
-config = {}
+// Tell express to use the body-parser middleware and to not parse extended bodies
+app.use(bodyParser.urlencoded({ extended: false }))
 
-config.TWILIO_ACCOUNT_SID = 'AC2a06129cd26f3c6d741e9a63e273e21e'
-config.TWILIO_AUTH_TOKEN = '07fe7810fa9bd93e7cba188a2d663d88'
 
-const app = express();
+app.post('/sms', (req) => {
+    const body = req.body.Body.split(/\r?\n/)
 
-app.post('/sms', (req, res) => {
+    
 
-  console.log("Body" + req.body);
-
-  const twiml = new MessagingResponse();
-
-  twiml.message('The Robots are coming! Head for the hills!');
-
-  res.writeHead(200, {'Content-Type': 'text/xml'});
-  res.end(twiml.toString());
+    axios.post('https://reqres.in/api/users', {
+        name: body[0],
+        job: body[1]
+      },
+      {
+      headers: {
+        Authorization: "This will be a hash",
+      }
+    })
+      .then((res) => {
+        console.log(res)
+      })
+      .catch((error) => {
+        console.error(error)
+      })  
 });
 
 http.createServer(app).listen(port, () => {
