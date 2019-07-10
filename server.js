91fb1553e370d6b1f95e9e8ad9f700c698880292
Tim Schartman <tschartman@yahoo.com>
2019-07-09 21:23:23 -0500

    twilio setup

--- server.js
+++ server.js
@@ -2,6 +2,9 @@ const http = require('http');
 const express = require('express');
 const MessagingResponse = require('twilio').twiml.MessagingResponse;
 
+config.TWILIO_ACCOUNT_SID = 'AC2a06129cd26f3c6d741e9a63e273e21e'
+config.TWILIO_AUTH_TOKEN = '07fe7810fa9bd93e7cba188a2d663d88'
+
 const app = express();
 
 app.post('/sms', (req, res) => {
