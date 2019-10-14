34ae05c2ee24721f156a4e368fd36a13a64ce4e9
Tim Schartman <tschartman@yahoo.com>
2019-10-14 16:34:14 -0500

    Fixed and scaled for new project

--- server.js
+++ server.js
@@ -4,33 +4,28 @@ const http = require('http');
 const express = require('express');
 const bodyParser = require('body-parser')
 var port = process.env.PORT || 8080;
+require('dotenv').config();
 
 const app = express()
+
+const accountSid = process.env.TWILIO_ACCOUNT_SID;
+const authToken = process.env.TWILIO_AUTH_TOKEN;
+var client = require('twilio')(accountSid, authToken);
 
 // Tell express to use the body-parser middleware and to not parse extended bodies
 app.use(bodyParser.urlencoded({ extended: false }))
 
 
-app.post('/sms', (req) => {
-    const body = req.body.Body.split(/\r?\n/)
-
+app.get('/sms', (req, res) => {    
+    client.messages
+    .create({
+       body: 'This is the ship that made the Kessel Run in fourteen parsecs?',
+       from: '+12563673096',
+       to: '+13096576541'
+     })
+    .then(message => console.log(message.sid));
     
-
-    axios.post('https://reqres.in/api/users', {
-        name: body[0],
-        job: body[1]
-      },
-      {
-      headers: {
-        Authorization: "This will be a hash",
-      }
-    })
-      .then((res) => {
-        console.log(res)
-      })
-      .catch((error) => {
-        console.error(error)
-      })  
+  res.send("Hey")
 });
 
 http.createServer(app).listen(port, () => {
